0ddb26bf98bb446a1ad30d420ec1f3b7279f66de
JIkaheimo <jaakko.o.ikaheimo@gmail.com>
2019-09-25 21:31:01 +0300

    Päivitä suoritukset.php käyttämään header- ja footer-komponenttia

--- suoritukset.php
+++ suoritukset.php
@@ -1,162 +1,132 @@
-<!DOCTYPE html>
-<html lang="fi">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title>Suoritukset | REENIKIRJA</title>
-    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
-  </head>
+<?php 
+  require_once(__DIR__.'/Komponentit/Header/header_kirjautunut.php'); 
+  HeaderKirjautunut('Suoritukset');
+?>
 
-  <body>
-    <header class="flex">
-      <nav class="kapea">
-        <a href="index_kirjautunut.php">REENIKIRJA</a>
-        <ul>
-          <li><a class="ei-valittu" href="ohjelmat.php">Ohjelmat</a></li>
-          <li><a class="valittu" href="suoritukset.php">Suoritukset</a></li>
-          <li><a class="ei-valittu" href="kayttajat.php">Käyttäjät</a></li>
-          <li class="oikealle">
-            <a class="ei-valittu" href="profiili.php">Profiili</a>
-          </li>
-          <li><a href="./Api/uloskirjaudu.php">Kirjaudu ulos</a></li>
-        </ul>
-      </nav>
-    </header>
+<header>
+  <h1>Suoritukset</h1>
+  <a class="nappi nappi-p" href="uusi_suoritus.php">Uusi suoritus +</a>
+</header>
 
-    <main class="kapea">
-      <header>
-        <h1>Suoritukset</h1>
-        <a class="nappi nappi-p" href="uusi_suoritus.php">Uusi suoritus +</a>
-      </header>
+<table>
+  <thead>
+    <tr>
+      <th>Päiväys</th>
+      <th>Harjoitus</th>
+      <th>Reeniohjelma</th>
+      <th>Kesto (min)</th>
+      <th></th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr class="suoritus-tr">
+      <th>21.09.2019</th>
+      <td>Rinta/ojentajat</td>
+      <td>
+        <a href="ohjelma.php?id=12"
+          >4-jakoinen kuntosaliohjelma edistyneille</a
+        >
+      </td>
+      <td>72</td>
 
-      <table>
-        <thead>
-          <tr>
-            <th>Päiväys</th>
-            <th>Harjoitus</th>
-            <th>Reeniohjelma</th>
-            <th>Kesto (min)</th>
-            <th></th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr class="suoritus-tr">
-            <th>21.09.2019</th>
-            <td>Rinta/ojentajat</td>
-            <td>
-              <a href="ohjelma.php?id=12"
-                >4-jakoinen kuntosaliohjelma edistyneille</a
-              >
-            </td>
-            <td>72</td>
+      <td>
+        <div class="sailio">
+          <a class="nappi nappi-p" href="muokkaa_suoritus.php?id=12"
+            >Muokkaa</a
+          >
+          <form action="./Api/poista_suoritus.php" method="POST">
+            <input type="hidden" id="12" name="suoritus" value="12" />
+            <button class="nappi-s" type="submit">Poista X</button>
+          </form>
+        </div>
+      </td>
+    </tr>
 
-            <td>
-              <div class="sailio">
-                <a class="nappi nappi-p" href="muokkaa_suoritus.php?id=12"
-                  >Muokkaa</a
-                >
-                <form action="./Api/poista_suoritus.php" method="POST">
-                  <input type="hidden" id="12" name="suoritus" value="12" />
-                  <button class="nappi-s" type="submit">Poista X</button>
-                </form>
-              </div>
-            </td>
-          </tr>
+    <tr class="suoritus-tr">
+      <th>18.09.2019</th>
+      <td>Juoksulenkki</td>
+      <td><a href="ohjelma.php?id=12">Kesäkuntoon 2050</a></td>
+      <td>63</td>
 
-          <tr class="suoritus-tr">
-            <th>18.09.2019</th>
-            <td>Juoksulenkki</td>
-            <td><a href="ohjelma.php?id=12">Kesäkuntoon 2050</a></td>
-            <td>63</td>
+      <td>
+        <div class="sailio">
+          <a class="nappi nappi-p" href="muokkaa_suoritus.php?id=16"
+            >Muokkaa</a
+          >
+          <form action="./Api/poista_suoritus.php" method="POST">
+            <input type="hidden" id="16" name="suoritus" value="16" />
+            <button class="nappi-s" type="submit">Poista X</button>
+          </form>
+        </div>
+      </td>
+    </tr>
 
-            <td>
-              <div class="sailio">
-                <a class="nappi nappi-p" href="muokkaa_suoritus.php?id=16"
-                  >Muokkaa</a
-                >
-                <form action="./Api/poista_suoritus.php" method="POST">
-                  <input type="hidden" id="16" name="suoritus" value="16" />
-                  <button class="nappi-s" type="submit">Poista X</button>
-                </form>
-              </div>
-            </td>
-          </tr>
+    <tr class="suoritus-tr">
+      <th>15.09.2019</th>
+      <td>Juoksulenkki</td>
+      <td>
+        <a href="ohjelma.php?id=12">Yleisurheilulla yleiskuntoon</a>
+      </td>
+      <td>63</td>
 
-          <tr class="suoritus-tr">
-            <th>15.09.2019</th>
-            <td>Juoksulenkki</td>
-            <td>
-              <a href="ohjelma.php?id=12">Yleisurheilulla yleiskuntoon</a>
-            </td>
-            <td>63</td>
+      <td>
+        <div class="sailio">
+          <a class="nappi nappi-p" href="muokkaa_suoritus.php?id=15"
+            >Muokkaa</a
+          >
+          <form action="./Api/poista_suoritus.php" method="POST">
+            <input type="hidden" id="15" name="suoritus" value="15" />
+            <button class="nappi-s" type="submit">Poista X</button>
+          </form>
+        </div>
+      </td>
+    </tr>
 
-            <td>
-              <div class="sailio">
-                <a class="nappi nappi-p" href="muokkaa_suoritus.php?id=15"
-                  >Muokkaa</a
-                >
-                <form action="./Api/poista_suoritus.php" method="POST">
-                  <input type="hidden" id="15" name="suoritus" value="15" />
-                  <button class="nappi-s" type="submit">Poista X</button>
-                </form>
-              </div>
-            </td>
-          </tr>
+    <tr class="suoritus-tr">
+      <th>13.09.2019</th>
+      <td>Juoksulenkki</td>
+      <td>
+        <a href="ohjelma.php?id=12"
+          >4-jakoinen kuntosaliohjelma edistyneille</a
+        >
+      </td>
+      <td>63</td>
 
-          <tr class="suoritus-tr">
-            <th>13.09.2019</th>
-            <td>Juoksulenkki</td>
-            <td>
-              <a href="ohjelma.php?id=12"
-                >4-jakoinen kuntosaliohjelma edistyneille</a
-              >
-            </td>
-            <td>63</td>
+      <td>
+        <div class="sailio">
+          <a class="nappi nappi-p" href="muokkaa_suoritus.php?id=90"
+            >Muokkaa</a
+          >
+          <form action="./Api/poista_suoritus.php" method="POST">
+            <input type="hidden" id="90" name="suoritus" value="90" />
+            <button class="nappi-s" type="submit">Poista X</button>
+          </form>
+        </div>
+      </td>
+    </tr>
 
-            <td>
-              <div class="sailio">
-                <a class="nappi nappi-p" href="muokkaa_suoritus.php?id=90"
-                  >Muokkaa</a
-                >
-                <form action="./Api/poista_suoritus.php" method="POST">
-                  <input type="hidden" id="90" name="suoritus" value="90" />
-                  <button class="nappi-s" type="submit">Poista X</button>
-                </form>
-              </div>
-            </td>
-          </tr>
+    <tr class="suoritus-tr">
+      <th>11.09.2019</th>
+      <td>Juoksulenkki</td>
+      <td><a href="ohjelma.php?id=12">Kesäkutnoon 2050</a></td>
+      <td>63</td>
 
-          <tr class="suoritus-tr">
-            <th>11.09.2019</th>
-            <td>Juoksulenkki</td>
-            <td><a href="ohjelma.php?id=12">Kesäkutnoon 2050</a></td>
-            <td>63</td>
+      <td>
+        <div class="sailio">
+          <a class="nappi nappi-p" href="muokkaa_suoritus.php?id=7"
+            >Muokkaa</a
+          >
+          <form action="./Api/poista_suoritus.php" method="POST">
+            <input type="hidden" id="7" name="suoritus" value="7" />
+            <button class="nappi-s" type="submit">Poista X</button>
+          </form>
+        </div>
+      </td>
+    </tr>
+  </tbody>
+</table>
 
-            <td>
-              <div class="sailio">
-                <a class="nappi nappi-p" href="muokkaa_suoritus.php?id=7"
-                  >Muokkaa</a
-                >
-                <form action="./Api/poista_suoritus.php" method="POST">
-                  <input type="hidden" id="7" name="suoritus" value="7" />
-                  <button class="nappi-s" type="submit">Poista X</button>
-                </form>
-              </div>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </main>
-
-    <footer>
-      <div>
-        <p>© REENIKIRJA 2019</p>
-        <p>
-          Sivuston toteutus ja ylläpito:
-          <a href="https://github.com/JIkaheimo">Jaakko Ikäheimo</a>
-        </p>
-      </div>
-    </footer>
-  </body>
-</html>
+ <?php 
+  require_once(__DIR__.'/Komponentit/footer.php');
+  Footer();
+?>
