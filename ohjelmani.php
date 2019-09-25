92f07c0ecff9e52155680f229664d466e756c8b5
JIkaheimo <jaakko.o.ikaheimo@gmail.com>
2019-09-25 21:24:02 +0300

    Luo ohjelmille tr-komponentti ja päivitä ohjelmani.php käyttämään sitä

--- ohjelmani.php
+++ ohjelmani.php
@@ -2,7 +2,7 @@
   require_once(__DIR__.'/Komponentit/Header/header_kirjautunut.php'); 
   HeaderKirjautunut('Ohjelmani');
 ?>
-      
+
 <header>
   <h1>Ohjelmani</h1>
   <a class="nappi nappi-p" href="uusi_ohjelma.php">Uusi ohjelma +</a>
@@ -18,77 +18,15 @@
     </tr>
   </thead>
   <tbody>
-    <tr class="ohjelma-tr">
-      <th>4-jakoinen saliohjelma edistyneille</th>
-      <td>5</td>
-      <td>vaikea</td>
 
-      <td>
-        <div class="sailio">
-          <a class="nappi nappi-p" href="muokkaa_ohjelma.php?id=12"
-            >Muokkaa</a
-          >
-          <form action="./Api/poista_ohjelma.php">
-            <input type="hidden" name="id" id="ohjelma-12" value="12" />
-            <button class="nappi-s" type="submit">Poista x</button>
-          </form>
-        </div>
-      </td>
-    </tr>
+    <?php
+      require_once(__DIR__.'/Komponentit/Ohjelmat/ohjelma_tr.php');
+      OhjelmaTR('4-jakoinen saliohjelma edistyneille', 5, 'vaikea', 12);
+      OhjelmaTR('4-jakoinen saliohjelma edistyneille', 4, 'helppo', 23);
+      OhjelmaTR('4-jakoinen saliohjelma edistyneille', 3, 'aloittelija', 15);
+      OhjelmaTR('24/7 treeniohjelma pähkähulluille', 20, 'extreme', 13);
+    ?>
 
-    <tr class="ohjelma-tr">
-      <th>4-jakoinen saliohjelma edistyneille</th>
-      <td>4</td>
-      <td>helppo</td>
-
-      <td>
-        <div class="sailio">
-          <a class="nappi nappi-p" href="muokkaa_ohjelma.php?id=23"
-            >Muokkaa</a
-          >
-          <form action="./Api/poista_ohjelma.php">
-            <input type="hidden" name="id" id="ohjelma-23" value="23" />
-            <button class="nappi-s" type="submit">Poista x</button>
-          </form>
-        </div>
-      </td>
-    </tr>
-
-    <tr class="ohjelma-tr">
-      <th>4-jakoinen saliohjelma edistyneille</th>
-      <td>3</td>
-      <td>aloittelija</td>
-
-      <td>
-        <div class="sailio">
-          <a class="nappi nappi-p" href="muokkaa_ohjelma.php?id=15"
-            >Muokkaa</a
-          >
-          <form action="./Api/poista_ohjelma.php">
-            <input type="hidden" name="id" id="ohjelma-15" value="15" />
-            <button class="nappi-s" type="submit">Poista x</button>
-          </form>
-        </div>
-      </td>
-    </tr>
-
-    <tr class="ohjelma-tr">
-      <th>24/7 treeniohjelma pähkähulluille</th>
-      <td>20</td>
-      <td>extreme</td>
-
-      <td>
-        <div class="sailio">
-          <a class="nappi nappi-p" href="muokkaa_ohjelma.php?id=13"
-            >Muokkaa</a
-          >
-          <form action="./Api/poista_ohjelma.php">
-            <input type="hidden" name="id" id="ohjelma-13" value="13" />
-            <button class="nappi-s" type="submit">Poista x</button>
-          </form>
-        </div>
-      </td>
-    </tr>
   </tbody>
 </table>
     
