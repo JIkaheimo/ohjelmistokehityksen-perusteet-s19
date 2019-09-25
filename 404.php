327436caedbc662f8d97e51f49bb1c520090b0c7
JIkaheimo <jaakko.o.ikaheimo@gmail.com>
2019-09-25 19:30:57 +0300

    Päivitä 404.php käyttämään header- ja footer-komponenttia

--- 404.php
+++ 404.php
@@ -1,53 +1,12 @@
-<!DOCTYPE html>
-<html lang="fi">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-    <title>Error 404 | REENIKIRJA</title>
-    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon" />
-  </head>
+<?php 
+  require_once(__DIR__.'/Komponentit/Header/header_vieras.php'); 
+  HeaderVieras('Error 404');
+?>
 
-  <body>
-    <header>
-      <nav class="kapea">
-        <a href="index.php">REENIKIRJA</a>
-        <form class="oikealle flex" action="./Api/kirjaudu.php" method="POST">
-          <div>
-            <label for="kayttajatunnus">Käyttäjätunnus:</label>
-            <input
-              type="text"
-              name="kayttajatunnus"
-              id="kayttajatunnus"
-              required=""
-            />
-          </div>
+<h1>Virhe 404</h1>
+<p>Voihan rähmä... Hakemaasi kohdetta ei valitettavasti voitu löytää. :(</p>
 
-          <div>
-            <label for="salasana">Salasana:</label>
-            <input type="password" name="salasana" id="salasana" required="" />
-          </div>
-
-          <button class="nappi-p" type="submit">Kirjaudu</button>
-        </form>
-      </nav>
-    </header>
-
-    <main class="kapea">
-      <h1>Virhe 404</h1>
-      <p>
-        Voihan rähmä... Hakemaasi kohdetta ei valitettavasti voitu löytää. :(
-      </p>
-    </main>
-
-    <footer>
-      <div>
-        <p>© REENIKIRJA 2019</p>
-        <p>
-          Sivuston toteutus ja ylläpito:
-          <a href="https://github.com/JIkaheimo">Jaakko Ikäheimo</a>
-        </p>
-      </div>
-    </footer>
-  </body>
-</html>
+<?php 
+  require_once(__DIR__.'/Komponentit/footer.php');
+  Footer();
+?>
