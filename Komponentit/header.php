19e31a32c3a687b0c2e008cc0e156ed02729f85c
JIkaheimo <jaakko.o.ikaheimo@gmail.com>
2019-09-25 19:01:22 +0300

    Lisää header-komponentti

---
+++ Komponentit/header.php
@@ -0,0 +1,14 @@
+<?php function Header_($otsikko) { ?>
+
+<!DOCTYPE html>
+<html lang="fi">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <meta http-equiv="X-UA-Compatible" content="ie=edge" />
+    <title><?=$otsikko?> | REENIKIRJA</title>
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+  </head>
+  <body>
+   
+<?php } ?>
