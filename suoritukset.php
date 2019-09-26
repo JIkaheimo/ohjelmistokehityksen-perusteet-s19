9f725e7f34888519d069da7a85993d702c1de17e
JIkaheimo <jaakko.o.ikaheimo@gmail.com>
2019-09-26 10:03:54 +0300

    Päivitä suoritukset.php käyttämään suoritus-tr-komponenttia

--- suoritukset.php
+++ suoritukset.php
@@ -19,114 +19,20 @@
     </tr>
   </thead>
   <tbody>
-    <tr class="suoritus-tr">
-      <th>21.09.2019</th>
-      <td>Rinta/ojentajat</td>
-      <td>
-        <a href="ohjelma.php?id=12"
-          >4-jakoinen kuntosaliohjelma edistyneille</a
-        >
-      </td>
-      <td>72</td>
+    <?php
+      // TODO: Hae suoritukset päivämäärän mukaan jäsrjestettynä tietokannasta
+      require_once(__DIR__.'/Komponentit/Suoritukset/suoritus_tr.php');
 
-      <td>
-        <div class="sailio">
-          <a class="nappi nappi-p" href="muokkaa_suoritus.php?id=12"
-            >Muokkaa</a
-          >
-          <form action="./Api/poista_suoritus.php" method="POST">
-            <input type="hidden" id="12" name="suoritus" value="12" />
-            <button class="nappi-s" type="submit">Poista X</button>
-          </form>
-        </div>
-      </td>
-    </tr>
-
-    <tr class="suoritus-tr">
-      <th>18.09.2019</th>
-      <td>Juoksulenkki</td>
-      <td><a href="ohjelma.php?id=12">Kesäkuntoon 2050</a></td>
-      <td>63</td>
-
-      <td>
-        <div class="sailio">
-          <a class="nappi nappi-p" href="muokkaa_suoritus.php?id=16"
-            >Muokkaa</a
-          >
-          <form action="./Api/poista_suoritus.php" method="POST">
-            <input type="hidden" id="16" name="suoritus" value="16" />
-            <button class="nappi-s" type="submit">Poista X</button>
-          </form>
-        </div>
-      </td>
-    </tr>
-
-    <tr class="suoritus-tr">
-      <th>15.09.2019</th>
-      <td>Juoksulenkki</td>
-      <td>
-        <a href="ohjelma.php?id=12">Yleisurheilulla yleiskuntoon</a>
-      </td>
-      <td>63</td>
-
-      <td>
-        <div class="sailio">
-          <a class="nappi nappi-p" href="muokkaa_suoritus.php?id=15"
-            >Muokkaa</a
-          >
-          <form action="./Api/poista_suoritus.php" method="POST">
-            <input type="hidden" id="15" name="suoritus" value="15" />
-            <button class="nappi-s" type="submit">Poista X</button>
-          </form>
-        </div>
-      </td>
-    </tr>
-
-    <tr class="suoritus-tr">
-      <th>13.09.2019</th>
-      <td>Juoksulenkki</td>
-      <td>
-        <a href="ohjelma.php?id=12"
-          >4-jakoinen kuntosaliohjelma edistyneille</a
-        >
-      </td>
-      <td>63</td>
-
-      <td>
-        <div class="sailio">
-          <a class="nappi nappi-p" href="muokkaa_suoritus.php?id=90"
-            >Muokkaa</a
-          >
-          <form action="./Api/poista_suoritus.php" method="POST">
-            <input type="hidden" id="90" name="suoritus" value="90" />
-            <button class="nappi-s" type="submit">Poista X</button>
-          </form>
-        </div>
-      </td>
-    </tr>
-
-    <tr class="suoritus-tr">
-      <th>11.09.2019</th>
-      <td>Juoksulenkki</td>
-      <td><a href="ohjelma.php?id=12">Kesäkutnoon 2050</a></td>
-      <td>63</td>
-
-      <td>
-        <div class="sailio">
-          <a class="nappi nappi-p" href="muokkaa_suoritus.php?id=7"
-            >Muokkaa</a
-          >
-          <form action="./Api/poista_suoritus.php" method="POST">
-            <input type="hidden" id="7" name="suoritus" value="7" />
-            <button class="nappi-s" type="submit">Poista X</button>
-          </form>
-        </div>
-      </td>
-    </tr>
+      SuoritusTR('21.09.2019', 'Rinta/ojentajat', '4-jakoinen kuntosaliohjelma edistyneille', 72, 12);
+      SuoritusTR('18.09.2019', 'Juoksulenkki', 'Kesäkuntoon 2050', 63, 16); 
+      SuoritusTR('15.09.2019', 'Juoksulenkki', 'Yleisurheilulla yleiskuntoon', 63, 15); 
+      SuoritusTR('13.09.2019', 'Juoksulenkki', '4-jakoinen kuntosaliohjelma edistyneille', 63, 90); 
+      SuoritusTR('11.09.2019', 'Juoksulenkki', 'Kesäkutnoon 2050', 63, 7); 
+    ?>
   </tbody>
 </table>
 
- <?php 
+<?php 
   require_once(__DIR__.'/Komponentit/footer.php');
   Footer();
 ?>
