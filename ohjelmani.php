0e11e873c4a739786228ee0f153b46c54b5e4468
JIkaheimo <jaakko.o.ikaheimo@gmail.com>
2019-10-08 11:00:30 +0300

    Lisää käyttäjän ohjelmien haku tietokannasta

--- ohjelmani.php
+++ ohjelmani.php
@@ -1,36 +1,56 @@
 <?php 
-  require_once(__DIR__.'/Komponentit/Header/header_kirjautunut.php'); 
-  HeaderKirjautunut('Ohjelmani');
+
+// Komponentit
+require_once(__DIR__.'/Komponentit/Header/header.php'); 
+require_once(__DIR__.'/Komponentit/Ohjelmat/ohjelma_tr.php');
+
+// Varmistetaan että käyttäjä on kirjautunut
+if ($kayttaja == null)
+{
+  header('Location: 401.php');
+}
+
+Headeri('Ohjelmani');
+
+$ohjelmat = Ohjelmat::haeKayttajan($db, $kayttaja);
+
 ?>
 
 <header>
   <h1>Ohjelmani</h1>
-  <a class="nappi nappi-p" href="uusi_ohjelma.php">Uusi ohjelma +</a>
+  <a class='nappi nappi-p' href='uusi_ohjelma.php'>Uusi ohjelma +</a>
 </header>
 
-<table>
-  <thead>
-    <tr>
-      <th>Nimi</th>
-      <th>Harjoituksia</th>
-      <th>Vaikeustaso</th>
-      <th></th>
-    </tr>
-  </thead>
-  <tbody>
+<?php if (!empty($ohjelmat)): ?>
+  <!-- Tulostetaan käyttäjän ohjelmat jos niitä on -->
+  <table>
+    <thead>
+      <tr>
+        <th>Nimi</th>
+        <th>Harjoituksia</th>
+        <th>Vaikeustaso</th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php
+        foreach ($ohjelmat as $ohjelma) { 
+          OhjelmaTR($ohjelma);
+        } 
+      ?>
+    </tbody>
+  </table>
 
-    <?php
-      require_once(__DIR__.'/Komponentit/Ohjelmat/ohjelma_tr.php');
-      OhjelmaTR('4-jakoinen saliohjelma edistyneille', 5, 'vaikea', 12);
-      OhjelmaTR('4-jakoinen saliohjelma edistyneille', 4, 'helppo', 23);
-      OhjelmaTR('4-jakoinen saliohjelma edistyneille', 3, 'aloittelija', 15);
-      OhjelmaTR('24/7 treeniohjelma pähkähulluille', 20, 'extreme', 13);
-    ?>
+<?php else: ?>
+  <!-- Tulostetaan ohje jos käyttäjällä ei ole ohjelmia -->
+  
+  <p>Aloita luomalla uusi ohjelma.</p>
+<?php endif; ?>
 
-  </tbody>
-</table>
-    
+<script src='./Scripts/ohjelmani.js'></script>
+  
 <?php 
   require_once(__DIR__.'/Komponentit/footer.php');
   Footer();
 ?>
+
