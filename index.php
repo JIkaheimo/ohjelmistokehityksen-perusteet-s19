77b0a7af3252de0a5eb1be83685e0db298991f03
JIkaheimo <jaakko.o.ikaheimo@gmail.com>
2019-10-08 10:58:03 +0300

    Näytä nyt index.php eri sisältö kirjautuneelle ja vierailevalle käyttäjälle, eri sivujen sijaan

--- index.php
+++ index.php
@@ -1,19 +1,15 @@
-<?php 
-  require_once(__DIR__.'/Komponentit/Header/header_vieras.php'); 
-  HeaderVieras('Etusivu');
-?>
-
-<div class="keskella focus">
-  <header>
-    <h1>
-      Reenikirjalla pidät treeniohjelmasi ja suorituksesi samassa
-      paikassa...
-    </h1>
-  </header>
-  <a class="nappi nappi-p" href="rekisteroidy.php">Rekisteröidy</a>
-</div>
-
-<?php 
-  require_once(__DIR__.'/Komponentit/footer.php');
-  Footer();
+<?php
+  require_once(__DIR__.'/Komponentit/Header/header.php');
+  Headeri('Etusivu');
+  
+  // Näytetään etusivulla eri näkymä vierailijoille ja kirjautuneille.
+  if ($kayttaja)
+  {
+    require_once(__DIR__.'/Komponentit/etusivu_kirjautunut.php');
+  }
+  else
+  {
+    require_once(__DIR__.'/Komponentit/etusivu_vierailija.php');
+  }
+  
 ?>
